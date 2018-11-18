778bbf386030dedafa8a6f370dce7ab6f49e6388
Suarmin Rais <suarmin.rais@gmail.com>
2018-11-19 01:46:09 +0700

    setting

---
+++ resources/js/app.js
@@ -0,0 +1,15 @@
+
+/**
+ * First we will load all of this project's JavaScript dependencies which
+ * includes Vue and other libraries. It is a great starting point when
+ * building robust, powerful web applications using Vue and Laravel.
+ */
+
+require('./bootstrap');
+
+window.Vue = require('vue');
+
+import Buefy from 'buefy';
+Vue.use(Buefy);
+
+// Vue.component('example-component', require('./components/ExampleComponent.vue'));
